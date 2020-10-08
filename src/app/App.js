6ee24464c0a041e29d02763ba3e7971d61c4a7a9
LiYuan1229 <823260355@qq.com>
2020-10-08 21:54:46 +0800

    [liyuan]: finish calculator

--- src/app/App.js
+++ src/app/App.js
@@ -3,14 +3,16 @@ import './app.less';
 import {Route, BrowserRouter} from "react-router-dom";
 import Home from "./Home";
 import Header from '../components/Header';
+import Calculator from '../components/Calculator';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-        <Header />
-          <Route exact path='/' component={Home}/>
+          <Header />
+            <Route exact path='/' component={Home}/>
+            <Route path='/calculator' component={Calculator}/> 
         </BrowserRouter>
       </div>
     );
